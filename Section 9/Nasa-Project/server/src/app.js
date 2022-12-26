cead62c12100027418c3391cc546020de465bb3a
MartinBurton <martin_burton@hotmail.com>
2022-12-26 12:22:28 +0000

    Tie into Launches Model

--- Section 9/Nasa-Project/server/src/app.js
+++ Section 9/Nasa-Project/server/src/app.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const express = require('express');
+
 const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
+
 const cors = require('cors');
 const morgan = require('morgan');
 
@@ -16,6 +19,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(planetsRouter);
+app.use(launchesRouter);
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
