f09aebbe6ac3c18c1b28004b46647986261a0460
MartinBurton <martin_burton@hotmail.com>
2023-01-11 13:52:18 +0000

    Added PM2 to Project

--- Section 10/Perf_Server/server.js
+++ Section 10/Perf_Server/server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const cluster = require('cluster');
+const os = require('os');
+
+const PORT = 3000;
 
 const app = express();
 
@@ -14,12 +18,27 @@ app.get('/', (req, res) => {
   // These funcitons are blocking
   // JSON.stringify({}) => "{}"
   // JSON.parse("{}") => {}
-  res.send('Performance Example');
+  // [5,1,2,3,4].sort()
+  res.send(`Performance Example: ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
   delay(9000);
-  res.send('Ding ding ding');
+  res.send(`Ding ding ding: ${process.pid}`);
 });
 
-app.listen(3000);
+// console.log('Running Server.js');
+if (cluster.isMaster) {
+  console.log('Master has been started...')
+  const NUM_WORKERS = os.cpus().length;
+  console.log(`Number of Workers: ${NUM_WORKERS}`);
+  for(let i=0; i < NUM_WORKERS; i++) {
+    cluster.fork();
+  };
+} else {
+  console.log('Worker Process started');
+  app.listen(PORT, () => {
+    console.log(`Worker is listening on ${PORT}`);
+  });
+}
+
